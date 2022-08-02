d12a079d545401b1093b43846c15080bd553b8dd
brake <riaconoo@icloud.com>
2022-08-02 17:57:20 +1000

    added 'AUTO INCREMENT' to 'employee' table in company_db database

--- db/schema.sql
+++ db/schema.sql
@@ -19,7 +19,7 @@ CREATE TABLE employee_role (
 );
 
 CREATE TABLE employee (
-    id INT,
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
     role_id INT,
